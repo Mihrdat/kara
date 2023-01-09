7038525279965a1aa85fdb20c64904a20d6166d6
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-01-09 17:07:38 +0330

    Build API for creating OTPs

---
+++ otp/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import OTP
+
+User = get_user_model()
+
+
+class CreateOTPSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(write_only=True)
+    user_id = serializers.IntegerField(read_only=True)
+
+    def validate_phone_number(self, phone_number):
+        if not User.objects.filter(phone_number=phone_number).exists():
+            raise serializers.ValidationError(
+                'No user with the given phone number was found.')
+        return phone_number
+
+    def create(self, validated_data):
+        phone_number = validated_data['phone_number']
+        user = User.objects.get(phone_number=phone_number)
+        return OTP.objects.create(user=user)
--- otp/views.py
+++ otp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import CreateOTPSerializer
 
-# Create your views here.
+
+@api_view(['POST'])
+def create_otp(request):
+    if request.method == 'POST':
+        serializer = CreateOTPSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
