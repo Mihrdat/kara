79d40c4ce78da48992893cac0e31ebf859832df7
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-01-09 16:14:59 +0330

    Add phone number field to User model

--- core/models.py
+++ core/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
