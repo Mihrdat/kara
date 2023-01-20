ddc29e717f1affc623311ac2c4bb52d35539f963
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-01-20 17:50:19 +0330

    Create Cart, Order and Customer models

--- store/models.py
+++ store/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator
+from uuid import uuid4
 
 
 class Category(models.Model):
@@ -12,3 +15,38 @@ class Product(models.Model):
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey(
         Category, on_delete=models.PROTECT, related_name='products')
+
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    created_at = models.DateField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)])
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='items')
+
+    class Meta:
+        unique_together = [['cart', 'product']]
+
+
+class Customer(models.Model):
+    birth_date = models.DateField(null=True, blank=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    placed_at = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    order = models.ForeignKey(
+        Order, on_delete=models.PROTECT, related_name='items')
