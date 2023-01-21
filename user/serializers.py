7446bab75a8c84c17014fc8ae25d63bad1753423
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-01-21 16:38:30 +0330

    Convert to viewset

--- user/serializers.py
+++ user/serializers.py
@@ -1,22 +1,41 @@
 from django.contrib.auth import get_user_model
+from django.core.cache import cache
 from rest_framework import serializers
 from .utils import is_valid_phone_number
 
 User = get_user_model()
 
 
-class CreateOTPSerializer(serializers.Serializer):
+class SendOTPSerializer(serializers.Serializer):
     phone_number = serializers.CharField()
 
     def validate_phone_number(self, phone_number):
         if not is_valid_phone_number(phone_number):
             raise serializers.ValidationError('Please enter a valid number.')
+
+        if cache.get(key=phone_number):
+            raise serializers.ValidationError(
+                'You have just sent a request. If you have not received the code, please wait until you can send another request.')
+
         return phone_number
 
 
 class VerifySerializer(serializers.Serializer):
     phone_number = serializers.CharField()
     code = serializers.CharField()
+
+    def validate(self, attrs):
+        phone_number = attrs['phone_number']
+        code = attrs['code']
+        value = cache.get(key=phone_number)
+
+        if value is None:
+            raise serializers.ValidationError(
+                'First, request to send an OTP code to this phone number.')
+        elif value != code:
+            raise serializers.ValidationError('The given code is invalid.')
+
+        return attrs
 
 
 class UserSerializer(serializers.ModelSerializer):
--- user/views.py
+++ user/views.py
@@ -1,11 +1,12 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 from django.contrib.auth import login, get_user_model
 from django.core.cache import cache
-from .serializers import CreateOTPSerializer, VerifySerializer, UserSerializer
+from store.models import Customer
+from .serializers import SendOTPSerializer, VerifySerializer, UserSerializer
 from .utils import generate_random_code
 
 User = get_user_model()
@@ -13,35 +14,34 @@ User = get_user_model()
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
     permission_classes = [IsAdminUser]
 
+    @action(detail=False, methods=['POST'], permission_classes=[AllowAny])
+    def send_otp(self, request):
+        serializer = SendOTPSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        phone_number = serializer.validated_data['phone_number']
+        code = generate_random_code(number_of_digits=6)
+        cache.set(key=phone_number, value=code, timeout=2 * 60)
+        print(code)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['POST'])
-def create(request):
-    serializer = CreateOTPSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    phone_number = serializer.validated_data['phone_number']
+    @action(detail=False, methods=['POST'], permission_classes=[AllowAny])
+    def verify(self, request):
+        serializer = VerifySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        phone_number = serializer.validated_data['phone_number']
 
-    if cache.get(key=phone_number):
-        return Response({'detail': 'You have just sent a request. If you have not received the code, please wait until you can send another request.'})
+        (user, is_created) = User.objects.get_or_create(phone_number=phone_number)
+        if is_created:
+            Customer.objects.create(user=user)
 
-    code = generate_random_code(number_of_digits=6)
-    cache.set(key=phone_number, value=code, timeout=(2 * 60))
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+        login(request, user)
+        return Response({'user_id': user.id}, status=status.HTTP_200_OK)
 
-
-@api_view(['POST'])
-def verify(request):
-    serializer = VerifySerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    phone_number = serializer.validated_data['phone_number']
-    code = serializer.validated_data['code']
-    value = cache.get(key=phone_number)
-
-    if value != code:
-        return Response({'detail': 'The given code is invalid.'})
-
-    (user, is_created) = User.objects.get_or_create(phone_number=phone_number)
-    login(request, user)
-    return Response({'user_id': user.id}, status=status.HTTP_200_OK)
+    def get_serializer_class(self):
+        if self.action == 'send_otp':
+            return SendOTPSerializer
+        if self.action == 'verify':
+            return VerifySerializer
+        return UserSerializer
