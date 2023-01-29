b04e0b423176ad67fad8181ab747a6ce73d6aae7
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-01-30 00:47:06 +0330

    Remove unnecessary code related to debug-toolbar

--- kara/settings/dev.py
+++ kara/settings/dev.py
@@ -4,10 +4,6 @@ SECRET_KEY = 'django-insecure-v*!upq!^puv^gzfj6&lnssm5q7iu2jgl$vue=iqwe!x*@z#3^$
 
 DEBUG = True
 
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': lambda request: True
-}
-
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
 }
