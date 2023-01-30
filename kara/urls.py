baef90352484020259b1b85beb504f304298dcd4
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-01-30 22:41:46 +0330

    Build cart-items API without nested-router library

--- kara/urls.py
+++ kara/urls.py
@@ -13,8 +13,15 @@ router.register('products', store_views.ProductViewSet)
 router.register('carts', store_views.CartViewSet)
 router.register('orders', store_views.OrderViewSet, basename='orders')
 
+cart_items_list = store_views.CartItemViewSet.as_view(
+    {'get': 'list', 'post': 'create'})
+cart_items_detail = store_views.CartItemViewSet.as_view(
+    {'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path('api/v1/', include(router.urls)),
+    path('api/v1/carts/<uuid:cart_pk>/items/', cart_items_list),
+    path('api/v1/carts/<uuid:cart_pk>/items/<int:pk>/', cart_items_detail),
 ]
