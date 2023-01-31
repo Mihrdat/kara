f6065420fc6d84ce206f68880564d48edd937bba
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-01-31 15:49:51 +0330

    Add cart-items route

--- kara/urls.py
+++ kara/urls.py
@@ -11,17 +11,12 @@ router.register('customers', user_views.CustomerViewSet)
 router.register('collections', store_views.CollectionViewSet)
 router.register('products', store_views.ProductViewSet)
 router.register('carts', store_views.CartViewSet)
+router.register(
+    'carts/(?P<cart_pk>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/items', store_views.CartItemViewSet)
 router.register('orders', store_views.OrderViewSet, basename='orders')
-
-cart_items_list = store_views.CartItemViewSet.as_view(
-    {'get': 'list', 'post': 'create'})
-cart_items_detail = store_views.CartItemViewSet.as_view(
-    {'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path('api/v1/', include(router.urls)),
-    path('api/v1/carts/<uuid:cart_pk>/items/', cart_items_list),
-    path('api/v1/carts/<uuid:cart_pk>/items/<int:pk>/', cart_items_detail),
 ]
