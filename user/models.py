d03a48404dfdae5df931017c211cf4aad3d4edd2
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-04-01 11:51:08 +0330

    Customize the User model creation

--- user/models.py
+++ user/models.py
@@ -1,9 +1,38 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager as BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, phone_number, password, **extra_fields):
+        if not phone_number:
+            raise ValueError('The given phone_number must be set')
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(phone_number, password, **extra_fields)
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(phone_number, password, **extra_fields)
 
 
 class User(AbstractUser):
+    objects = UserManager()
     phone_number = models.CharField(max_length=11, unique=True)
     username = models.CharField(max_length=150, blank=True, null=True)
 
